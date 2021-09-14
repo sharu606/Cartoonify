908eb62082fcae4a3ab47ae2438be1e083e0834f
S Sharanya <sridharan784@gmail.com>
2021-09-14 13:01:57 +0530

    Read the image and plot it

--- source code.py
+++ source code.py
@@ -1,6 +1,8 @@
+import cv2
 import easygui  # to open the filebox
 import tkinter as tk
 from tkinter import *
+import matplotlib.pyplot as plt
 
 top = tk.Tk()
 top.geometry('500x500')
@@ -10,6 +12,16 @@ top.configure(background='white')
 
 def upload():
     ImagePath = easygui.fileopenbox()
+    cartoonify(ImagePath)
+
+
+def cartoonify(imagePath: str):
+    image = cv2.imread(imagePath)
+    image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGB)
+    resized1 = cv2.resize(image, (1000, 1000))
+
+    plt.imshow(resized1, cmap='gray')
+    plt.show()
 
 
 upload = Button(top, text="Cartoonify an Image", command=upload, padx=10, pady=5, background='#000000',
